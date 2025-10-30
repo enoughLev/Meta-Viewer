e93b4f3e21a41b0ae4c7b7bde79a00071a8e0543
enoughLev <levchik-z@mail.ru>
2025-10-30 22:50:39 +0700

    add file choose

--- ObservWindow.py
+++ ObservWindow.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QFileDialog
 
 from GaleryWindow import GalleryWindow
 from Forms.ObservForm_ui import Ui_MainWindow
@@ -12,18 +12,26 @@ class ObserverWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Главное окно")
         self.setupUi(self)
 
+        self.file_path = ""
+
         self.open_gallery_btn.clicked.connect(self.open_gallery)
+        self.choose_file_btn.clicked.connect(self.choose_file)
 
         self.gallery_form = GalleryWindow()
 
         self.image_viewer = ImageViewer()
         self.layout.addWidget(self.image_viewer)
         self.img_view = ImageViewer()
-        self.image_viewer.setImage("C:\\Users\levch\Desktop\qwe.png")
+        #self.image_viewer.setImage("C:\\Users\levch\Desktop\qwe.png")
 
 
     def open_gallery(self):
         self.gallery_form.show()
+
+    def choose_file(self):
+        self.file_path = QFileDialog.getOpenFileName(self, 'Выбор файла', "C:\\Users\levch\Desktop", 'Изображения (*.png *.jpg)')
+        print(self.file_path)
+        self.image_viewer.setImage(self.file_path[0])
 
 
 
